f48075280f6c0f7f7c91d4443cd3af29dc50fdae
Gabriel Judá <90941036+Gabrieljuda1@users.noreply.github.com>
2023-10-01 21:58:24 -0300

    upload front e backend

---
+++ backend.js/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
+
+const usuarios = [];
+
+app.post('/cadastrar', (req, res) => {
+    const { nome, sobrenome, email, senha, data } = req.body;
+    usuarios.push({ nome, sobrenome, email, senha, data });
+    res.status(200).send('Usuário cadastrado com sucesso.');
+});
+
+app.get('/usuarios', (req, res) => {
+    res.json(usuarios);
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
